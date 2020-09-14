ab0f09b4ee02818b23ac0af5c983b8bf8431e6c2
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-14 21:34:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array) 
+  counter = 0 
+  if counter < array.length 
+    while counter < array.length 
+      yield
+      counter += 1 
+    end
+  else
+end
+array
+end
+    
 
